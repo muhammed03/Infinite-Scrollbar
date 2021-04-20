ccfc5c1c5e329656efd3104f2732bdb073547858
Muhammed Abdrahman <muhammed03amin@gmail.com>
2021-04-20 15:55:04 +0600

    feat: add filter function

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const filter = document.getElementById("filter");
 let limit = 5;
 let page = 1;
 
-//Fetch posts from API
+// Fetch posts from API
 async function getPosts() {
   const res = await fetch(
     `https://jsonplaceholder.typicode.com/posts?_limit=${limit}&_page=${page}`
@@ -19,14 +19,16 @@ async function getPosts() {
 // Show posts in DOM
 async function showPosts() {
   const posts = await getPosts();
+
   posts.forEach((post) => {
     const postEl = document.createElement("div");
     postEl.classList.add("post");
-    postEl.innerHTML = `<div class="number">${post.id}</div>
-        <div class="post-info">
-            <h2 class="post-title">${post.title}</h2>
-            <p class=""post-body>${post.body}</p>
-        </div>
+    postEl.innerHTML = `
+      <div class="number">${post.id}</div>
+      <div class="post-info">
+        <h2 class="post-title">${post.title}</h2>
+        <p class="post-body">${post.body}</p>
+      </div>
     `;
 
     postsContainer.appendChild(postEl);
@@ -43,10 +45,28 @@ function showLoading() {
     setTimeout(() => {
       page++;
       showPosts();
-    });
+    }, 300);
   }, 1000);
 }
 
+// Filter posts by input
+function filterPosts(e) {
+  const term = e.target.value.toUpperCase();
+  const posts = document.querySelectorAll(".post");
+
+  posts.forEach((post) => {
+    const title = post.querySelector(".post-title").innerText.toUpperCase();
+    const body = post.querySelector(".post-body").innerText.toUpperCase();
+
+    if (title.indexOf(term) > -1 || body.indexOf(term) > -1) {
+      post.style.display = "flex";
+    } else {
+      post.style.display = "none";
+    }
+  });
+}
+
+// Show initial posts
 showPosts();
 
 window.addEventListener("scroll", () => {
@@ -56,3 +76,5 @@ window.addEventListener("scroll", () => {
     showLoading();
   }
 });
+
+filter.addEventListener("input", filterPosts);
